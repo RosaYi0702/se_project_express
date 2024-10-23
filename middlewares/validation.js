b5ec92458a9c2988b432eb486af59a4588af07d9
RosaYi <rosayihuilin@gmail.com>
2024-10-23 12:59:46 -0400

    4th submit

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -16,7 +16,7 @@ module.exports.validateCardBody = celebrate({
       "string.max": 'The maximum length of the "name" field is 30',
       "string.empty": 'The "name" field must be filled in',
     }),
-    imagUrl: Joi.string().required().custom(validateURL).messages({
+    imageUrl: Joi.string().required().custom(validateURL).messages({
       "string.empty": 'The "imageUrl" field must be filled in',
       "string.uri": 'the "imageUrl" field must be a valid url',
     }),
